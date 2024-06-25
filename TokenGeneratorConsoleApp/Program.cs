07d81126a1359ce9bddb5058b57aeed78685b355
Piyush Dubey (DevBox) <piyushdubey07@gmail.com>
2024-06-25 18:37:30 -0400

    Adding app

---
+++ TokenGeneratorConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Azure.Core;
+using Azure.Identity;
+
+// Application / Client Id
+string clientId = "";
+
+// Tenant Id
+string tenantId = "";
+
+// Client Secret
+string clientSecret = "";
+
+// Create a client secret credential
+var clientSecretCredential = new ClientSecretCredential(
+    tenantId, clientId, clientSecret);
+
+// Get a token for the Storage account
+var tokenRequestContext = new TokenRequestContext(new[] { "https://storage.azure.com/.default" });
+var accessToken = await clientSecretCredential.GetTokenAsync(tokenRequestContext);
+
+Console.WriteLine($"Access Token: {accessToken.Token}");
+ 
